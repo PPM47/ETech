fc9a433a61feed2c6c35d364132199ed9000f6fa
Pavan Malshan <71922536+PPM47@users.noreply.github.com>
2024-07-25 14:21:06 +0530

    v2.0.4

--- .eslintrc.js
+++ .eslintrc.js
@@ -2,11 +2,12 @@ const { rules } = require('eslint-config-prettier')
 
 module.exports = {
   root: true,
-  extends: ['plugin:@next/next/recommended', '@payloadcms'],
+  extends: ['plugin:@next/next/recommended', '@payloadcms', 'prettier'],
   ignorePatterns: ['**/payload-types.ts'],
-  plugins: ['prettier'],
+  plugins: ['prettier', 'simple-import-sort'],
   rules: {
     'prettier/prettier': ['error', { endOfLine: 'auto' }],
+    'simple-import-sort/imports': 'error',
     'no-console': 'off',
     // 'import/no-extraneous-dependencies': 'off',
     // 'import/prefer-default-export': 'off',
@@ -14,3 +15,4 @@ module.exports = {
     // 'import/no-cycle': 'off',
   },
 }
+

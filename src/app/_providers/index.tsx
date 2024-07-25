43b2cc7d133264fd99d5f9ae0948bba151a876eb
Pavan Malshan <71922536+PPM47@users.noreply.github.com>
2024-07-25 13:41:56 +0530

    v2.0.1

--- src/app/_providers/index.tsx
+++ src/app/_providers/index.tsx
@@ -1,20 +1,22 @@
 'use client'
 
 import React from 'react'
-
 import { AuthProvider } from '../_providers/Auth'
 import { CartProvider } from '../_providers/Cart'
 import { ThemeProvider } from './Theme'
 import { FilterProvider } from './Filter'
 
+// This component wraps its children with multiple context providers
 export const Providers: React.FC<{
   children: React.ReactNode
 }> = ({ children }) => {
   return (
-    <ThemeProvider>
-      <AuthProvider>
-        <FilterProvider>
-          <CartProvider>{children}</CartProvider>
+    <ThemeProvider> {/* Provides theme context to all children */}
+      <AuthProvider> {/* Provides authentication context */}
+        <FilterProvider> {/* Provides filter context */}
+          <CartProvider> {/* Provides cart context */}
+            {children} {/* Render children with all contexts applied */}
+          </CartProvider>
         </FilterProvider>
       </AuthProvider>
     </ThemeProvider>
